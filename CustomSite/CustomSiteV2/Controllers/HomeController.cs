8c5e244725e6743f510a6e9c64a03c660a5d1e42
blackasninja <ash_winp@hotmail.com>
2016-03-18 18:34:38 +1300

    Add bootstrap, jquery and other

---
+++ CustomSite/CustomSiteV2/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Web.Mvc;
+
+namespace CustomSiteV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ CustomSite/CustomSiteV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomSiteV2.MvcApplication" Language="C#" %>
