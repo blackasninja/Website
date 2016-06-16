a67c701bd7adb1f0e29f76a81de3601891a077c6
Ashwin Patel <ash_winp@hotmail.com>
2016-06-16 21:38:40 +1200

    Style changes and fixed routeing

--- CustomSiteV2/CustomSite_V2/App_Start/RouteConfig.cs
+++ CustomSiteV2/CustomSite_V2/App_Start/RouteConfig.cs
@@ -18,12 +18,7 @@ namespace CustomSite_V2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new
-                {
-                    controller = "Home",
-                    action = "Index",
-                    id = UrlParameter.Optional,
-                }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional,}
             );
         }
     }
